6ad44ceb097b10a9e286024ae01ab7fa462f2792
CodyConck <codyconck@gmail.com>
2021-06-23 17:05:18 -0500

    allowed user to create data

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 //requires the data from animals.json file in data folder
 const { animals } = require('./data/animals');
 
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -49,7 +55,46 @@ function findById(id, animalsArray) {
     return result;
 }
 
-//route that the front end can request data from
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  
+  //writes incoming data to animals.json
+  //synchronous method that doesn't require callback function
+  fs.writeFileSync(
+    //writes animals.json file in data subdirectory using path.join() method which
+    //joins value of __dirname which is the directory of the file we execute the code in
+    //with the path to the animals.json file
+    path.join(__dirname, './data/animals.json'),
+    //jsonstringify converts javascript array data so we can save it as JSON
+    //null argument means we dont want to edit existing data
+    //2 arg indicates that we want to create white space b/t values to make it more readable
+    JSON.stringify({ animals: animalsArray}, null, 2)
+  );
+
+  //return finished code to post route for response
+  return animal;
+}
+
+//validation function to take new animal data from req.body and check if each key exists
+//and that its the right type of data
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+//"GET request/route"
+//route that the front end can request data from.
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -65,6 +110,22 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+});
+//"POST request/route"
+//sets up a route on the server that accepts data (from user) to be used/stored server-side.
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+    } else {
+
+    //add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
 
 app.listen(PORT, () => {
